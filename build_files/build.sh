38f7b82e0fd31298ca2b6c9df47be2b3c74246b5
Mike Mikulak <mpmikulak@gmail.com>
2026-06-01 20:36:06 -0500

    Adds a few new programs to test the pipeline

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,12 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y kitty
 
+flatpak install -y app.zen_browser.zen \
+    org.libretro.RetroArch \
+    com.bitwarden.desktop \
+    com.discordapp.Discord
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
